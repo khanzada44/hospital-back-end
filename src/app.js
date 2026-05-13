35c056a4313b14fc4f80ec00ef354f60438712e1
Minham44 <minhamkhanzada4455@gmail.com>
2026-05-13 19:06:29 +0500

    added the error middleware.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const routes = require("./routes/index");
 const cookieParser = require("cookie-parser");
 const authmiddleware = require("./common/middleware/auth.middleware");
+const errorMiddleware = require("./common/middleware/error.middleware");
 
 const app = express();
 
@@ -14,6 +15,10 @@ app.use(
   })
 );
 
+app.use(morgan("combined"));
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
@@ -23,10 +28,7 @@ app.use(
   })
 );
 
-app.use(morgan("combined"));
-
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(cookieParser());
 
 
 app.get("/health", (req, res) => {
@@ -37,30 +39,7 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/", routes);
-app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to Hospital Backend API"
-  });
-});
-
-
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route Not Found"
-  });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({
-    success: false,
-    message: "Internal Server Error",
-    error: err.message
-  });
-});
-
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ src/common/middleware/error.middleware.js
@@ -0,0 +1,36 @@
+const AppError = require("./utils/AppError");
+
+app.use((err, req, res, next) => {
+
+    let statusCode = err.statusCode || 500;
+    let message = err.message || "Internal Server Error";
+
+    console.error("ERROR");
+    console.error(err);
+
+    if (err instanceof AppError) {
+        return res.status(statusCode).json({
+            success: false,
+            message
+        });
+    }
+
+    if (err.code === "23505") {
+        return res.status(409).json({
+            success: false,
+            message: "Duplicate field value"
+        });
+    }
+
+    if (err.code === "22P02") {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid input format"
+        });
+    }
+
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+    });
+});
